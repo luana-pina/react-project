168db25cb3cba699f721fafb90884bfc8188ad0c
Luana Pina <83229362+luana-pina@users.noreply.github.com>
2022-03-17 17:44:56 -0300

    fix: bugs in filters automated test

--- cypress/integration/filters.spec.js
+++ cypress/integration/filters.spec.js
@@ -5,55 +5,37 @@
 /// <reference types="cypress" />
 
 describe("Filters in home page", () => {
-  describe("single filters", () => {
-    beforeEach(() => {
-      localStorage.setItem(
-        "bearer",
-        "MTI5.s-yNAcHW5ck2sriVfvj3wImEgRM9BbRDcAqU-Cp_FIkEZWCy9gghSjAx_fLo"
-      );
-      cy.intercept("GET", "**/bet/all-bets").as("recentGamesReq");
-      cy.intercept("GET", "**/cart_games").as("gamesReq");
-      cy.visit("http://localhost:3000/home");
-      cy.wait("@gamesReq")
-        .then(({ response }) => {
-          expect(response.body).has.property("types");
-          Cypress.env("numberOfGames", response.body.types.length);
-        })
-        .its("response.statusCode")
-        .should("eq", 200);
-      cy.wait("@recentGamesReq").its("response.statusCode").should("eq", 200);
-    });
+  beforeEach(() => {
+    localStorage.setItem(
+      "bearer",
+      "MTI5.s-yNAcHW5ck2sriVfvj3wImEgRM9BbRDcAqU-Cp_FIkEZWCy9gghSjAx_fLo"
+    );
+  });
+  it("single filters", () => {
+    cy.intercept("GET", "**/bet/all-bets").as("recentGamesReq");
+    cy.intercept("GET", "**/cart_games").as("gamesReq");
+    cy.visit("http://localhost:3000/home");
+    cy.wait("@gamesReq")
+      .then(({ response }) => {
+        expect(response.body).has.property("types");
+        Cypress.env("numberOfGames", response.body.types.length);
+      })
+      .its("response.statusCode")
+      .should("eq", 200);
+    cy.wait("@recentGamesReq").its("response.statusCode").should("eq", 200);
     Cypress._.times(Cypress.env("numberOfGames"), (k) => {
-      it(`filter ${k + 1}`, () => {
-        cy.get(`#\\3${k + 1} `).click();
-        cy.get(`#\\3${k + 1} `).click();
-      });
+      cy.get(`#\\3${k + 1} `).click();
+      cy.get(`#\\3${k + 1} `).click();
     });
   });
-  describe("multiple filters", () => {
-    beforeEach(() => {
-      localStorage.setItem(
-        "bearer",
-        "MTI5.s-yNAcHW5ck2sriVfvj3wImEgRM9BbRDcAqU-Cp_FIkEZWCy9gghSjAx_fLo"
-      );
-    });
+  it("multiple filters", () => {
     Cypress._.times(Cypress.env("numberOfGames"), (k) => {
-      it(`${k + 1} filters `, () => {
-        cy.get(`#\\3${k + 1} `).click();
-      });
+      cy.get(`#\\3${k + 1} `).click();
     });
   });
-  describe("any filter", () => {
-    beforeEach(() => {
-      localStorage.setItem(
-        "bearer",
-        "MTI5.s-yNAcHW5ck2sriVfvj3wImEgRM9BbRDcAqU-Cp_FIkEZWCy9gghSjAx_fLo"
-      );
-    });
+  it("any filter", () => {
     Cypress._.times(Cypress.env("numberOfGames"), (k) => {
-      it(`unselect ${k + 1} filters `, () => {
-        cy.get(`#\\3${k + 1} `).click();
-      });
+      cy.get(`#\\3${k + 1} `).click();
     });
   });
 });
